73ba2b0366a1a0ad34bc406477ef664ba7940911
long <longtrinhvan97@gmail.com>
2020-09-15 22:01:21 +0700

    tuesday ,september

--- projectSpringMvc/src/main/java/com/springmvc/config/JPAConfig.java
+++ projectSpringMvc/src/main/java/com/springmvc/config/JPAConfig.java
@@ -1,10 +1,24 @@
 package com.springmvc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
 public class JPAConfig {
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPersistenceUnitName("persistence-data");
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+		return em;
+	}
 
 }
