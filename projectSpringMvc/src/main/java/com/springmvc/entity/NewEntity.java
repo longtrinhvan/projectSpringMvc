fe384a086565de201858f5a5a633d3980e84410f
long <longtrinhvan97@gmail.com>
2020-09-15 21:48:26 +0700

    tuesday, september

--- projectSpringMvc/src/main/java/com/springmvc/entity/NewEntity.java
+++ projectSpringMvc/src/main/java/com/springmvc/entity/NewEntity.java
@@ -1,10 +1,70 @@
 package com.springmvc.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="new")
+@Table(name="news")
 public class NewEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "thumbnail")
+	private String thumbnail;
+	
+	@Column(name="shortdescription")
+	private String shortDescription;
+	
+	@Column(name = "content")
+	private String content;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
 }
