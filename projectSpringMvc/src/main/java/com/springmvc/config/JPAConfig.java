f1782c9ed7392f26c7ec28d528349e5d33eb528c
long <longtrinhvan97@gmail.com>
2020-09-15 22:07:16 +0700

    tuesday, september

--- projectSpringMvc/src/main/java/com/springmvc/config/JPAConfig.java
+++ projectSpringMvc/src/main/java/com/springmvc/config/JPAConfig.java
@@ -2,6 +2,7 @@ package com.springmvc.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
