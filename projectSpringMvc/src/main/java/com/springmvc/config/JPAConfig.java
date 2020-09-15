7a763bf0d861ea89820a6a7560bf58c7a850a449
long <longtrinhvan97@gmail.com>
2020-09-15 22:05:03 +0700

    tuesday ,september

--- projectSpringMvc/src/main/java/com/springmvc/config/JPAConfig.java
+++ projectSpringMvc/src/main/java/com/springmvc/config/JPAConfig.java
@@ -1,6 +1,9 @@
 package com.springmvc.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -20,5 +23,13 @@ public class JPAConfig {
 		em.setJpaProperties(additionalProperties());
 		return em;
 	}
-
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/jspservletjdbc");
+		dataSource.setUsername("root");
+		dataSource.setPassword("123456");
+		return dataSource;
+	}
 }
